f77e36565e86b51b626f370e3e8fe1dabde9c4ce
Kross <krossevruiz@gmail.com>
2018-01-23 08:09:41 -0800

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AndroidPermissions } from '@ionic-native/android-permissions';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+		AndroidPermissions,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AndroidPermissions } from '@ionic-native/android-permissions';
+//import * as socketio from 'socket.io-client';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +9,56 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	// sock = null;
+  //
+	// peerConnection = null;
+
+  constructor(
+		public navCtrl: NavController,
+    private androidPermissions: AndroidPermissions
+	) {
+    console.log("hello??");
+
+    this.androidPermissions.requestPermissions([this.androidPermissions.PERMISSION.CAMERA, this.androidPermissions.PERMISSION.RECORD_AUDIO]);
+    this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.CAMERA).then(
+      success => console.log("Hey you have permission"),
+      err => {console.log("Uh oh"); this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.CAMERA);}
+    );
+
+    this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.RECORD_AUDIO).then(
+      success => console.log("Hey you have permission"),
+      err => {console.log("Uh oh"); this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.RECORD_AUDIO);}
+    );
+
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+      .then(stream => {
+        console.log("got here");
+        let video = document.getElementById('user-video-element');
+        (<HTMLVideoElement>video).srcObject = stream;
+      })
+      .catch(e => {
+        console.log("MESSAGE");
+        console.log(e);
+        console.log("EOMESSAGE");
+      });
+		//const SERVER_ADDRESS = 'http://5cf0c64f.ngrok.io';
+		// const PC_Configuration = {};
+		//this.sock = socketio(SERVER_ADDRESS);
+		// this.peerConnection = new RTCPeerConnection(PC_Configuration);
+
+		//this.sock.on('omae_wa_mou_shindeiru', () => {
+		//	console.log("NANI?")
+		//});
+
+		// navigator.mediaDevices.getUserMedia({video: true, audio: false})
+		//   .then( stream => {
+		// 	     let video = document.getElementById('user-video-element');
+		// 			 (<HTMLVideoElement>video).srcObject = stream;
+    //        console.log(video.srcObject);
+		// 	})
+		// 	.catch(err => {
+		// 	     console.log("Error occured" + err);
+		// 	});
 
   }
 
